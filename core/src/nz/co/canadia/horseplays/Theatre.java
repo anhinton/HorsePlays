daa19c3659d7bf9201e4fe4a5af16b9a163b278e
Ashley Noel Hinton <ashley@canadia.co.nz>
2017-07-22 16:03:31 +1200

    switching between all of my shots

--- core/src/nz/co/canadia/horseplays/Theatre.java
+++ core/src/nz/co/canadia/horseplays/Theatre.java
@@ -1,9 +1,10 @@
 package nz.co.canadia.horseplays;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Timer;
 
 import nz.co.canadia.horseplays.util.Constants;
@@ -12,7 +13,7 @@ import nz.co.canadia.horseplays.util.Constants;
  * The theatre in which we stage our play
  */
 
-class Theatre {
+class Theatre implements InputProcessor {
 
     private Backdrop backdrop;
     private Spotlight spotlight01;
@@ -20,10 +21,9 @@ class Theatre {
     private TheatreStage stage;
     private Texture horseTexture01;
     private Texture horseCloseTexture01;
-    private Horse horse01;
     private Texture horseTexture02;
     private Texture horseCloseTexture02;
-    private Horse horse02;
+    private Array<Horse> horses;
     private Curtains curtains;
     private Horse currentHorse;
 
@@ -32,8 +32,11 @@ class Theatre {
     private boolean animating;
 
     private int clickCounter;
+    private boolean touchDown;
 
     Theatre() {
+        Gdx.input.setInputProcessor(this);
+
         stage = new TheatreStage(0, 0);
         backdrop = new Backdrop(Constants.APP_WIDTH / 2, stage.getHeight());
         spotlight01 = new Spotlight(
@@ -46,53 +49,27 @@ class Theatre {
                 true);
         horseTexture01 = new Texture(Gdx.files.internal("graphics/horse01.png"));
         horseCloseTexture01 = new Texture(Gdx.files.internal("graphics/horseFace01.png"));
-        horse01 = new Horse(horseTexture01, horseCloseTexture01, stage.getHeight(), true,
-                Constants.Side.LEFT);
+        horses = new Array<Horse>();
+        horses.add(new Horse(horseTexture01, horseCloseTexture01, stage.getHeight(), true,
+                Constants.Side.LEFT));
         horseTexture02 = new Texture(Gdx.files.internal("graphics/horse02.png"));
         horseCloseTexture02 = new Texture(Gdx.files.internal("graphics/horseFace02.png"));
-        horse02 = new Horse(horseTexture02, horseCloseTexture02, stage.getHeight(), true,
-                Constants.Side.RIGHT);
+        horses.add(new Horse(horseTexture02, horseCloseTexture02, stage.getHeight(), true,
+                Constants.Side.RIGHT));
         curtains = new Curtains();
 
         currentTheatreScene = Constants.TheatreScene.START;
         animating = false;
         currentZoomLevel = Constants.ZoomLevel.WIDE;
-        currentHorse = horse01;
+        currentHorse = horses.get(0);
         clickCounter = 0;
+        touchDown = false;
     }
 
     void update() {
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            switch (currentTheatreScene) {
-                case START:
-                    startShow();
-                    break;
-                case OPENING:
-                    break;
-                case PERFORMING:
-                    if (clickCounter <= 3) {
-                        clickCounter++;
-                        switch (currentZoomLevel) {
-                            case WIDE:
-                                currentZoomLevel = Constants.ZoomLevel.CLOSE;
-                                break;
-                            case CLOSE:
-                                currentZoomLevel = Constants.ZoomLevel.WIDE;
-                                break;
-                        }
-                    } else {
-                        endShow();
-                    }
-                    break;
-                case CLOSING:
-                    break;
-                case FINISHED:
-                    break;
-            }
-        }
 
         // check animation state
-        animating = curtains.isMoving() | horse01.isMoving() | horse02.isMoving();
+        animating = horsesMoving() | curtains.isMoving();
 
         // advance to next theatre scene
         switch (currentTheatreScene) {
@@ -110,16 +87,18 @@ class Theatre {
 
         // update objects
         curtains.update();
-        horse01.update();
-        horse02.update();
+        for (Horse horse : horses) {
+            horse.update();
+        }
     }
 
     void draw (SpriteBatch batch) {
         switch (currentZoomLevel) {
             case WIDE:
                 backdrop.draw(batch);
-                horse01.draw(batch, currentZoomLevel);
-                horse02.draw(batch, currentZoomLevel);
+                for (Horse horse : horses) {
+                    horse.draw(batch, currentZoomLevel);
+                }
                 stage.draw(batch);
                 spotlight01.draw(batch);
                 spotlight02.draw(batch);
@@ -145,16 +124,18 @@ class Theatre {
         Timer.schedule(new Timer.Task() {
             @Override
             public void run() {
-                horse01.enter();
-                horse02.enter();
+                for (Horse horse : horses) {
+                    horse.enter();
+                }
             }
         }, 2);
         currentTheatreScene = Constants.TheatreScene.OPENING;
     }
 
     private void endShow() {
-        horse01.exit();
-        horse02.exit();
+        for (Horse horse : horses) {
+            horse.exit();
+        }
         Timer.schedule(new Timer.Task() {
             @Override
             public void run() {
@@ -162,5 +143,91 @@ class Theatre {
             }
         }, 2);
         currentTheatreScene = Constants.TheatreScene.CLOSING;
+    }
+
+    private boolean horsesMoving() {
+        for (Horse horse : horses) {
+            if (horse.isMoving()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (!touchDown) {
+            touchDown = true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        if (touchDown) {
+            touchDown = false;
+            switch (currentTheatreScene) {
+                case START:
+                    startShow();
+                    break;
+                case OPENING:
+                    break;
+                case PERFORMING:
+                    if (clickCounter <= 3) {
+                        clickCounter++;
+                        switch (currentZoomLevel) {
+                            case WIDE:
+                                currentZoomLevel = Constants.ZoomLevel.CLOSE;
+                                if (clickCounter == 1) {
+                                    currentHorse = horses.get(1);
+                                } else {
+                                    currentHorse = horses.get(0);
+                                }
+                                break;
+                            case CLOSE:
+                                currentZoomLevel = Constants.ZoomLevel.WIDE;
+                                break;
+                        }
+                    } else {
+                        endShow();
+                    }
+                    break;
+                case CLOSING:
+                    break;
+                case FINISHED:
+                    break;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
