c65bcab934cc940695e1ac54456096697f521600
Ashley Noel Hinton <ashley@canadia.co.nz>
2021-02-09 15:18:00 +1300

    Implement IOSFontLoader

--- ios/src/nz/co/canadia/horseplays/IOSLauncher.java
+++ ios/src/nz/co/canadia/horseplays/IOSLauncher.java
@@ -11,7 +11,7 @@ public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new HorsePlays(), config);
+        return new IOSApplication(new HorsePlays(new IOSFontLoader()), config);
     }
 
     public static void main(String[] argv) {
